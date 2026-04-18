b6d687a7ec803df54b772210f6e519912bfa00ad
lychee75869 <15924357826@163.com>
2026-04-18 20:31:52 +0800

    basic mode

---
+++ src/process/process.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "process.h"
+using namespace std;
+
+void Process::planProcess() {
+    cout << "this is planning process" << endl;
+    my_map.mapInfo();
+    cout << "planning successful" << endl;
+
+}
